762c55506d268908113e1079e3ece3b0cd26bdfc
mohamed <mohamed.araby288@gmail.com>
2022-10-31 16:42:32 +0200

    Adding Video Details

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import {Stack,Box} from '@mui/material';
-import {VideoCard,ChannelCard} from './';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos }) => {
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return <Loader />;
+  
   return (
-    <div>
-      <Stack direction="row" flexWrap="wrap"
-      justifyContent="start" gap={2}>
-
-       {videos.map((item,idx)=>(
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
         <Box key={idx}>
-            {item.id.videoId && <VideoCard video={item} />}
-            {item.id.channelId && <ChannelCard channelDetial={item} />}
-
+          {item.id.videoId && <VideoCard video={item} /> }
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-       ))}
-      </Stack>
-    </div>
-  )
+      ))}
+    </Stack>
+  );
 }
 
-export default Videos
+export default Videos;
